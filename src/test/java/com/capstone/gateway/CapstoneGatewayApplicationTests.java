71877d5497c956f6185b7bc8397a9c17d1eedd0c
Shahad7 <shahadneeliyatt786@gmail.com>
2024-08-29 17:52:39 +0530

    shift

---
+++ src/test/java/com/capstone/gateway/CapstoneGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
